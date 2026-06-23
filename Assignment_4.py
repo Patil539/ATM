59276480df76d6e4832e7aaabcc6cff47bfd262e
Janhavi Patil <janupatil539@gmail.com>
2026-06-23 20:18:23 +0530

    Add files via upload

---
+++ Assignment_4.py
@@ -0,0 +1,29 @@
+# def bill(units,rate=8):
+#     return units*rate
+#
+# units=int(input("Enter units: "))
+# print("Total Bill: ",bill(units))
+
+import hashlib
+
+def register_user(name, email, password):
+    if "@" not in email:
+        print("Invalid Email")
+        return
+
+    if len(password) < 8:
+        print("Password too short")
+        return
+
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+
+    print("Registration Successful")
+    print("Name:", name)
+    print("Email:", email)
+    print("Hashed Password:", hashed)
+
+name = input("Enter Name: ")
+email = input("Enter Email: ")
+password = input("Enter Password: ")
+
+register_user(name, email, password)
